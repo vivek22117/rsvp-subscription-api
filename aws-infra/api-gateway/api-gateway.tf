5672f5efd156f4bd146b6130b5f22305448a86f8
vivek22117 <vivekmishra22117@gmail.com>
2020-10-16 16:56:01 +0530

    updating api gateway tf files

--- aws-infra/api-gateway/api-gateway.tf
+++ aws-infra/api-gateway/api-gateway.tf
@@ -1,4 +1,58 @@
 ############################################
+#     Configure API Gateway Logging         #
+############################################
+resource "aws_api_gateway_account" "rsvp_subscriber_api_config" {
+  cloudwatch_role_arn = aws_iam_role.rsvp_processor_api_role.arn
+}
+
+resource "aws_iam_role" "rsvp_processor_api_role" {
+  name = "RSVPSubscriberAPIGatewayRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "email_processor_policy" {
+  name = "RSVPSubscriberAPIGatewayPolicy"
+  role = aws_iam_role.rsvp_processor_api_role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:FilterLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+
+############################################
 #             API Gateway REST API         #
 ############################################
 resource "aws_api_gateway_rest_api" "rsvp_subscriber_api" {
@@ -92,6 +146,19 @@ resource "aws_api_gateway_method" "rsvp_api_method_POST" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method_settings" "enable_logging" {
+  rest_api_id = aws_api_gateway_rest_api.rsvp_subscriber_api.id
+  stage_name  = aws_api_gateway_deployment.rsvp_api_deployment.stage_name
+  method_path = "${aws_api_gateway_resource.rsvp_subscriber_api_resource.path_part}/${aws_api_gateway_method.rsvp_api_method_POST.http_method}"
+
+  settings {
+    metrics_enabled    = true
+    data_trace_enabled = true
+    logging_level      = "INFO"
+  }
+}
+
+
 resource "aws_api_gateway_method_response" "api_method_response_200" {
   depends_on = [aws_api_gateway_method.rsvp_api_method_POST]
 
@@ -129,5 +196,9 @@ resource "aws_api_gateway_deployment" "rsvp_api_deployment" {
     redeployment = sha1(join(",", list(
     jsonencode(aws_api_gateway_integration.rsvp_api_integration),
     )))
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
