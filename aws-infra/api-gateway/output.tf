0127b2c797e4279b9fbd47f313b7e1e378ba2ce4
vivek22117 <vivekmishra22117@gmail.com>
2020-10-17 15:34:25 +0530

    removing unwanted codes

--- aws-infra/api-gateway/output.tf
+++
@@ -1,8 +0,0 @@
-# URL to invoke the API
-output "url" {
-  value = aws_api_gateway_deployment.rsvp_api_deployment.invoke_url
-}
-
-output "execution_arn" {
-  value = aws_api_gateway_deployment.rsvp_api_deployment.execution_arn
-}
