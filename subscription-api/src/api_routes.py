c36a07dba40e9ed72335a64152154f5c14ad41f3
vivek22117 <vivekmishra22117@gmail.com>
2021-01-09 17:52:19 +0530

    api for health check

---
+++ subscription-api/src/api_routes.py
@@ -0,0 +1,30 @@
+from src import app
+
+import boto3
+import datetime
+import json
+import logging
+import os
+import requests
+import sys
+
+from flask import Response, request
+
+LOG_FILENAME = 'subscription_api_logs.out'
+CURRENT_VERSION = "v0.1"
+
+logging.basicConfig(filename=LOG_FILENAME, format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG,
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
+
+
+@app.route('/api/<string:version>/health', methods=['GET'])
+@by_path_counter
+def health(version):
+    if version == CURRENT_VERSION:
+        status = 200
+        response = {"health": "ok", "response": "ok"}
+    else:
+        status = 400
+        response = {"error": "invalid API version", "response": "ok"}
+    return Response(json.dumps(response), status=status, mimetype='application/json')
