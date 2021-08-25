c3ad25bc5f988c4eaa12be797127adcce5a2d9b2
vivek22117 <vivekmishra22117@gmail.com>
2021-08-25 11:30:42 +0530

    updating data TF file

--- aws-infra/lambda-resources/data.tf
+++ aws-infra/lambda-resources/data.tf
@@ -1,12 +1,12 @@
 ###################################################
 # Fetch remote state for S3 deployment bucket     #
 ###################################################
-data "terraform_remote_state" "backend" {
+data "terraform_remote_state" "s3_buckets" {
   backend = "s3"
 
   config = {
-    bucket ="${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
-    key = "state/${var.environment}/backend/terraform.tfstate"
+    bucket = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    key    = "state/${var.environment}/s3-buckets/terraform.tfstate"
     region = var.default_region
   }
 }
