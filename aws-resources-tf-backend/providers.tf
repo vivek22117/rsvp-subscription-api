53b49177ed19dfbf29cb7ee085515f3a7a716c01
vivek22117 <vivekmishra22117@gmail.com>
2021-11-23 12:05:10 +0530

    adding TF providers

---
+++ aws-resources-tf-backend/providers.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = var.default_region
+  #  profile = "default"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.3.0"
+    }
+  }
+
+  required_version = ">= 0.13"
+
+  backend "s3" {
+    #    profile        = "default"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
+
+data "aws_caller_identity" "current" {}
