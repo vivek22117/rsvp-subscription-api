75340e0551f893e1468af9208492b9db093819ab
vivek22117 <vivekmishra22117@gmail.com>
2022-01-14 18:03:47 +0530

    adding data resources to read s3 bucket

--- aws-tf-modules/modules.lambda-resources/data.tf
+++ aws-tf-modules/modules.lambda-resources/data.tf
@@ -1,12 +1,15 @@
 ###################################################
-# Fetch remote state for S3 deployment bucket     #
+#       Configuration to fetch remote state       #
+#       of artifactory bucket for deployment      #
 ###################################################
 data "terraform_remote_state" "s3_buckets" {
   backend = "s3"
 
   config = {
-    bucket = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    bucket = "${var.environment}-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
     key    = "state/${var.environment}/s3-buckets/terraform.tfstate"
     region = var.default_region
   }
 }
+
+data "aws_caller_identity" "current" {}
