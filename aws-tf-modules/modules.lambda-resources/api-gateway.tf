1650c5f037bd8c79213fea7236757528fd25de57
vivek22117 <vivekmishra22117@gmail.com>
2022-01-14 19:30:33 +0530

    adding new values in tfvars file

--- aws-tf-modules/modules.lambda-resources/api-gateway.tf
+++ aws-tf-modules/modules.lambda-resources/api-gateway.tf
@@ -144,7 +144,7 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,DELETE'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
